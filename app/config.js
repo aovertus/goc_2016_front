9c3bc13302eb0d7e8d95579cab0a2d17abb5ec9c
Justin Bériot <justin.beriot@gmail.com>
2016-04-09 01:19:19 +0200

    yo

--- app/config.js
+++ app/config.js
@@ -1,7 +1,20 @@
-angular.module('app', []).config(function(uiGmapGoogleMapApiProvider) {
-    uiGmapGoogleMapApiProvider.configure({
-        //    key: 'your api key',
-        v: '3.20', //defaults to latest 3.X anyhow
-        libraries: 'weather,geometry,visualization'
-    });
-});
+(function() {
+	'use strict';
+
+	angular
+		.module('app')
+		.config(config);
+
+	function config(uiGmapGoogleMapApiProvider, $mdThemingProvider) {
+		uiGmapGoogleMapApiProvider.configure({
+	        //    key: 'your api key',
+	        v: '3.20', //defaults to latest 3.X anyhow
+	        libraries: 'weather,geometry,visualization'
+    	});
+
+    	$mdThemingProvider.theme('default')
+    		.primaryPalette('indigo')
+    		.accentPalette('pink');
+	}
+
+})();
